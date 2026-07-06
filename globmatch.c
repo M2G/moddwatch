1fb6d55e26dc24c075a59c06b1b0b878ee3eb554
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-07-06 18:59:22 +0200

    fix: compile

--- globmatch.c
+++ globmatch.c
@@ -4,22 +4,24 @@
 // match_class
 
 static bool char_in_range(char lo, char hi, char c) {
-    return (unsigned char)log <= (unsigned char)c && (unsigned char)c <= (unsigned char)hi;
+    return (unsigned char)lo <= (unsigned char)c && (unsigned char)c <= (unsigned char)hi;
 }
 
 // traite un seul item de classe (soit "a-z", soit "a") et avance cls.
-static const char *match_class_item(const char *cls, chr c, bool *found) {
-    if (cls[1] == '-' && cls[2] != ']' && cls[2] != '\0') {}
+static const char *match_class_item(const char *cls, char c, bool *found) {
+    if (cls[1] == '-' && cls[2] != ']' && cls[2] != '\0') {
+        if (char_in_range(cls[0], cls[2], c)) *found = true;
+        return cls + 3;
+    }
 
-
-    // return cls; 
+    return cls; 
 }
 
 static const char *scan_class_body(const char *cls, char c, bool *found) {
     bool first = true;
     while(*cls && (*cls != ']' || first)){
         first = false;
-        cls = match_class_item(cls, c, found)
+        cls = match_class_item(cls, c, found);
     }
     return cls;
 }
