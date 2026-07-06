ed54beea86cdec2a16db95289dd1baf0dcf28c99
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-07-06 04:10:25 +0200

    feat: impl method scan_class_body, match_class and match_class_item (wip)

--- globmatch.c
+++ globmatch.c
@@ -8,9 +8,31 @@ static bool char_in_range(char lo, char hi, char c) {
 }
 
 // traite un seul item de classe (soit "a-z", soit "a") et avance cls.
+static const char *match_class_item(const char *cls, chr c, bool *found) {
+    if (cls[1] == '-' && cls[2] != ']' && cls[2] != '\0') {}
+
+
+    // return cls; 
+}
+
+static const char *scan_class_body(const char *cls, char c, bool *found) {
+    bool first = true;
+    while(*cls && (*cls != ']' || first)){
+        first = false;
+        cls = match_class_item(cls, c, found)
+    }
+    return cls;
+}
 
 static const char *match_class(const char *cls, char c, bool *matched) {
-    
+    bool negate = (*cls == '-' || *cls == '^');
+    if (negate) cls++;
+
+    bool found = false;
+    cls = scan_class_body(cls, c, *found);
+    if(*cls == ']') cls++;
+
+    // add ternary
 
 
     return cls;
