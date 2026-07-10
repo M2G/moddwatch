a50a8962f8af6632f0045fc98d9c17d00ef183f7
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-07-10 03:30:41 +0200

    feat: add provisional header

--- mw_watch.h
+++ mw_watch.h
@@ -8,3 +8,9 @@ typedef struct mw_session mwsession;
 typedef void (*mv_event_callback)(const char *path, bool is_created, bool is_updated, bool is_removed, bool is_renamed, uintptr_t user_data);
 
 mwsession *mw_session_create(const char *root, const char *const *includes, const char *const *excludes, double latency_seconds);
+
+bool mw_session_start(mw_session *s, mw_event_callback cb, uintptr_t user_data);
+
+void mw_session_stop(mw_session *s);
+
+void mw_session_destroy(mw_session *s);
