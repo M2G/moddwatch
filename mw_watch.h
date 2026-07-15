65d091a839a06b9cc88da429359d6c3de4d1e857
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-07-15 04:11:31 +0200

    feat: wrapp with extern C for compatibility

--- mw_watch.h
+++ mw_watch.h
@@ -3,6 +3,10 @@
 #define MW_WATCH_H
 #include <stdbool.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct mw_session mwsession;
 
 typedef void (*mv_event_callback)(const char *path, bool is_created, bool is_updated, bool is_removed, bool is_renamed, uintptr_t user_data);
@@ -14,3 +18,9 @@ bool mw_session_start(mw_session *s, mw_event_callback cb, uintptr_t user_data);
 void mw_session_stop(mw_session *s);
 
 void mw_session_destroy(mw_session *s);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
