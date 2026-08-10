f677f84a26ed67ed12ceb4fea71ca77c98d6ba37
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-08-10 03:36:04 +0200

    feat: define ds_utils.h

---
+++ ds_utils.h
@@ -0,0 +1,19 @@
+#ifndef DS_UTILS_H
+#define DS_UTILS_H
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+long ds_index_matched_closing_alt(const char *s, size_t len, bool allow_escaping);
+
+long ds_index_next_alt(const char *s, size_t len, bool allow_escaping);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
