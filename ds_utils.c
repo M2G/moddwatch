8ea60c030bf26543e7c222079ddec474962ae311
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-08-10 03:38:55 +0200

    feat: cpp -> c and define method

---
+++ ds_utils.c
@@ -0,0 +1,19 @@
+#include "ds_utils.h"
+
+long ds_index_matched_closing_alt(const char *s, size_t len, bool allow_escaping) {
+
+    int alts = 1;
+
+    for (size_t i = 0; i < len; i++) {}
+
+    return -1;
+}
+
+long ds_index_next_alt(const char *s, size_t len, bool allow_escaping) {
+
+    int alts = 1;
+
+    for (size_t i = 0; i < len; i++) {}
+
+    return -1;
+}
