3706ff6ff47aef2aff99b68ea2decd9024cc423a
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-08-10 18:47:18 +0200

    feat: add and tests every use case allow escaping (last case -> wip)

--- ds_utils.c
+++ ds_utils.c
@@ -1,13 +1,25 @@
 #include "ds_utils.h"
 
+#include <stdio.h>
+
 long ds_index_matched_closing_alt(const char *s, size_t len, bool allow_escaping) {
 
     int alts = 1;
 
     for (size_t i = 0; i < len; i++) {
-        if (allow_escaping && s[i] == '\\') {}
-        else if (allow_escaping && s[i] == '{') {}
-        else if (allow_escaping && s[i] == '}') {}
+        if (allow_escaping && s[i] == '\\') {
+            printf("\\ %c", s[i]);
+            i++;
+        }
+        else if (allow_escaping && s[i] == '{') {
+            printf("{ %c", s[i]);
+            alts++;
+        }
+        else if (allow_escaping && s[i] == '}') {
+            printf("} %c", s[i]);
+            alts--;
+        }
+        // ... last case
     }
 
     return -1;
@@ -18,10 +30,18 @@ long ds_index_next_alt(const char *s, size_t len, bool allow_escaping) {
     int alts = 1;
 
     for (size_t i = 0; i < len; i++) {
-        if (allow_escaping && s[i] == '\\') {}
-        else if (allow_escaping && s[i] == '{') {}
-        else if (allow_escaping && s[i] == '}') {}
-        else if (allow_escaping && s[i] == ',') {}
+        if (allow_escaping && s[i] == '\\') {
+            i++;
+        }
+        else if (allow_escaping && s[i] == '{') {
+            alts++;
+        }
+        else if (allow_escaping && s[i] == '}') {
+            alts--;
+        }
+        else if (allow_escaping && s[i] == ',') {
+            // ... last case
+        }
     }
 
     return -1;
