aa033164f7f5f03059c983a2f5411b19d0f3bc20
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-08-12 04:00:13 +0200

    feat: add algo do_match_with_separator (wip)

--- ds_match.cpp
+++ ds_match.cpp
@@ -21,12 +21,46 @@ namespace {
 
      bool is_zero_length_pattern(const char *pattern, size_t pat_len)
      {
-          // len 0 = true
-          // len >= 1 + pattern[0...3] == "*" = true
-          // len > 0 + pattern[0] == "{" = true
+          if (len == 0) return true;
+          if (len == 1 && pattern[0] == '*') return true;
+          if (len == 2 && pattern[0] == '*' && pattern[1] == '*') return true;
+          if (len == 3 && pattern[0] == SEPARATOR && pattern[1] == '*' && pattern[2] == '*') return true;
+          if (len == 3 && pattern[0] == '*' && pattern[1] == '*' && pattern[2] == SEPARATOR) return true;
+          if (len == 4 && pattern[0] == SEPARATOR && pattern[1] == '*' && pattern[2] == '*' && pattern[3] == SEPARATOR) return true;
+
+          if (len > 0 && pattern[0] == '{') {
+               // ...
+          }
+
+
+         return false;
      }
 
-     ds_result do_match_with_separator(){}
+     ds_result do_match_with_separator(
+          const char *pattern,
+          size_t pat_len,
+          const char *name,
+          size_t name_len,
+          long doublestar_pattern_backtrack,
+          long doublestar_name_backtrack,
+          long star_pattern_backtrack,
+          long star_name_backtrack,
+          size_t pat_idx,
+          size_t name_idx
+          ) {
+          bool start_of_segment = true;
+
+          while (name_idx < name_len) {
+               //...
+               if (pat_idx < pat_len) {
+                    char pc = pattern[pat_idx];
+                    if (pc == '*') {
+                         // ...
+                    }
+               }
+          }
+
+     }
 
 
 }
